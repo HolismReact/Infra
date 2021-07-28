7bea7b34ac67e9454325e2cad5b26e35684139b5
Saeed Nemati <sn.1361@gmail.com>
2021-07-28 13:10:11 +0430

    committed by script

--- src/Panel/Menu.js
+++ src/Panel/Menu.js
@@ -1,12 +1,38 @@
 import { Link } from 'react-router-dom';
 import menuItems from '../Menu.js';
 
+const menuItemRenderer = (menuItem) => 
+<>
+    {
+        menuItem.url
+        ?
+        <Link to={menuItem.url}>{menuItem.title}</Link>
+        :
+        <span>{menuItem.title}</span>
+    }
+</>
+
 const Menu = () => {
     return <>
         <ul>
             {
-                menuItems.map(item => <li key={item.url}>
-                        <Link to={item.url}>{item.title}</Link>
+                menuItems.map(item => <li key={item.url || item.title}>
+                    {
+                        item.items && item.items.length > 0
+                            ?
+                            <>
+                                <span>{item.title}</span>
+                                <ul>
+                                    {
+                                        item.items.map(childItem => <li key={childItem.url}>
+                                            {menuItemRenderer(childItem)}
+                                        </li>)
+                                    }
+                                </ul>
+                            </>
+                            :
+                            menuItemRenderer(item)
+                    }
                 </li>)
             }
         </ul>
