d1914223e99ee681c8a56577f224720b6c981f9e
pourmand <pourmand2010@gmail.com>
2021-07-31 21:21:43 +0430

    add cart & List

--- src/Components/Layouts/List.js
+++ src/Components/Layouts/List.js
@@ -15,21 +15,40 @@ const List = (props) => {
 
   const [listParameters, setListParameters] = useState(CreateListParameters());
 
-  return <>
-    <ListContext.Provider value={{
-      listParameters: listParameters,
-      reloadItems: () => { }
-    }}>
-      <div>{props.title}</div>
-      <Filtering filters={props.filters} />
-      <Sorting sorts={props.sorts} />
-      <ListActions actions={props.listActions} />
-      <Items entity={props.entity} headers={props.headers} row={props.row} card={props.card} />
-      {/* <div>
+  return  <div className="card">
+    <div className="card-header">
+      <h6 className="card-title">{props.title}</h6>
+      <ul className="list-inline card-tools">
+        <li className="list-inline-item mb-0">
+          <button type="button" class="btn btn-outline-success btn-uppercase">
+            <i class="fa fa-save"></i> <span className="hidden md:block" >Create User </span>
+          </button>
+        </li>
+        <li>
+          <button type="button" class="btn btn-outline-secondary btn-uppercase">
+            <i class="fa fa-plus"></i>  <span className="hidden md:block">Create User</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div className="card-body">
+      <ListContext.Provider value={{
+        listParameters: listParameters,
+        reloadItems: () => { }
+      }}>
+
+        <Filtering filters={props.filters} />
+        <Sorting sorts={props.sorts} />
+        <ListActions actions={props.listActions} />
+        <Items entity={props.entity} headers={props.headers} row={props.row} card={props.card} />
+        {/* <div>
         {data.map(item => <div key={item.id}>{item.id}</div>)}
       </div> */}
-    </ListContext.Provider>
-  </>
+      </ListContext.Provider>
+
+    </div>
+  </div>
+
 };
 
 export default List;
