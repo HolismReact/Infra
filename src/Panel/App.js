bde379a77f2f30225876b49c9ebcca1bdb060f2a
Saeed Nemati <sn.1361@gmail.com>
2021-08-03 11:35:13 +0430

    committed by script

--- src/Panel/App.js
+++ src/Panel/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import MainRouting from '../Base/MainRouting';
 import Sidebar from './Sidebar';
 import { useState } from 'react';
+import Header from './Header';
 
 function App() {
   const [sideBarIsOpen, setSidebarAsOpen] = useState();
 
   return <div className="flex">
     <Sidebar className="w-1/6 bg-white" />
-    <MainRouting className="flex-1" />
+    <div className="flex-1">
+      <Header />
+      <div className="p-10">
+        <MainRouting />
+      </div>
+    </div>
   </div>
 }
 
