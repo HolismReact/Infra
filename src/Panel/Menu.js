4714476fb3f05351274b93fe7de82805493fcec4
Saeed Nemati <sn.1361@gmail.com>
2021-08-05 18:27:54 +0430

    committed by script

--- src/Panel/Menu.js
+++ src/Panel/Menu.js
@@ -6,7 +6,7 @@ import Down from '../Components/Icons/Down.js';
 const liStyle = "h-10 hover:bg-gray-50 cursor-pointer text-sm tracking-wide text-gray-600 hover:text-gray-800 font-normal"
 const iconStyle = "text-gray-600 hover:text-gray-900 mr-3"
 
-const MenuItemWithSubmenu = ({item}) => {
+const MenuItemWithSubmenu = ({item, onClick}) => {
     const [isSubmenuOpen, setIsSubmenuOpen] = useState();
     const openSubmenu = () => {
         setIsSubmenuOpen(!isSubmenuOpen);
@@ -27,6 +27,7 @@ const MenuItemWithSubmenu = ({item}) => {
                     {
                         item.children.map((child) => {
                             return <Link
+                                onClick={onClick}
                                 to={child.url}
                                 key={child.url}
                                 className={liStyle + " flex items-center hover:bg-gray-100"}
@@ -42,12 +43,12 @@ const MenuItemWithSubmenu = ({item}) => {
     );
 };
 
-const Menu = () => {
+const Menu = ({onClick}) => {
     return <div id="menu" className="mt-5">
         {
             menuItems.map((item) => {
                 if (item.children && item.children.length > 0) {
-                    return <MenuItemWithSubmenu item={item} />
+                    return <MenuItemWithSubmenu item={item} onClick={onClick}/>
                 }
                 else {
                     if (!item.children && !item.url) {
@@ -57,7 +58,7 @@ const Menu = () => {
                         throw new Error('Please remove menu items with zero childrens. Empty children array is not valid.')
                     }
                     return (<Fragment key={item.title}>
-                        <Link to={item.url} className={liStyle + " flex items-center"}>
+                        <Link onClick={onClick} to={item.url} className={liStyle + " flex items-center"}>
                             <span className="px-9 flex items-cener">
                                 <span className={iconStyle}>
                                     <item.iconSvg />
