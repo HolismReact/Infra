d3260ff305ef8da4d367b64f4c0e0aa93f63ec2c
Saeed Nemati <sn.1361@gmail.com>
2021-08-30 15:37:39 +0430

    committed by script

--- src/Components/Browse/Browse.js
+++ src/Components/Browse/Browse.js
@@ -1,18 +1,73 @@
+import React, { useState, useEffet } from 'react';
 import Filtering from "../List/Filtering";
 import Sorting from "../List/Sorting";
 import Items from "../List/Items";
+import CreateListParameters from '../../Base/CreateListParameters';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import CachedIcon from '@material-ui/icons/Cached';
+import useLocalStorageState from '../../Base/UseLocalStorageState';
+import { ListContext } from '../List/List';
+
+const listActionIconStyle = "text-gray-700 hover:text-blue-500 cursor-pointer";
 
 const Browse = ({ sorts, filters, row, card, entity, headers }) => {
-    return <div>
-        <Filtering filters={filters} />
-        <Sorting sorts={sorts} />
+
+    const [listParameters, setListParameters] = useState(CreateListParameters(KeycloakClient.keycloak.subject, entity));
+    const [isFilteringOpen, setIsFilteringOpen] = useLocalStorageState(false, `${KeycloakClient.keycloak.subject}_${entity}_isFilteringOpen`);
+
+    const toggleFiltering = () => {
+        setIsFilteringOpen(!isFilteringOpen);
+    }
+
+    return <ListContext.Provider value={{
+        listParameters: listParameters,
+    }} id='list'>
+        <div className='flex items-center justify-end px-6 py-2'>
+            <div className="flex items-center">
+                {
+                    sorts
+                        ?
+                        <Sorting sorts={sorts} />
+                        :
+                        null
+                }
+                {
+                    filters && (filters.props?.children?.length > 0 || filters.props?.children?.props)
+                        ?
+                        <span
+                            id='showHideFiltering'
+                            className={listActionIconStyle + " mr-4"}
+                            onClick={toggleFiltering}><FilterListIcon /></span>
+                        :
+                        null
+                }
+                {
+                    <span
+                        id='reload'
+                        onClick={() => Holism.emit(Holism.reloadRequirement)}
+                        className={listActionIconStyle}
+                    >
+                        <CachedIcon />
+                    </span>
+                }
+            </div>
+        </div>
+
+        {
+            isFilteringOpen
+                ?
+                <Filtering filters={filters} />
+                :
+                null
+        }
+
         <Items
             entity={entity}
             headers={headers}
             row={row}
             card={card}
         />
-    </div>
+    </ListContext.Provider>
 }
 
 export { Browse };
