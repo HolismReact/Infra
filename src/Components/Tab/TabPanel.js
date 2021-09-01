48029ee909e05aadbbb65b78225c73d030dfcc01
“Sanam” <pourmand2010@gmail.com>
2021-09-01 08:59:09 +0430

    committed by script

--- src/Components/Tab/TabPanel.js
+++ src/Components/Tab/TabPanel.js
@@ -1,6 +1,4 @@
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 export default function TabPanel({ children, value, index, ...other }) 
 {
