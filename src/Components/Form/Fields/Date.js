ecd7a3dcf3dd9eb8a3d97aa0dacf5de751737a4e
Saeed Nemati <sn.1361@gmail.com>
2021-09-08 10:54:56 +0430

    committed by script

---
+++ src/Components/Form/Fields/Date.js
@@ -0,0 +1,15 @@
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+const Date = ({ }) => {
+  return <div>Date</div>
+}
+
+export { Date }
