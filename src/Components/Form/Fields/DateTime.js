331117720bbef8e7f220182a1f114dd0e50bfdd1
Saeed Nemati <sn.1361@gmail.com>
2021-09-08 12:08:56 +0430

    committed by script

--- src/Components/Form/Fields/DateTime.js
+++ src/Components/Form/Fields/DateTime.js
@@ -9,6 +9,12 @@ import {
 } from '@material-ui/pickers';
 
 const DateTime = ({ }) => {
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     return <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <KeyboardDatePicker
             margin="normal"
