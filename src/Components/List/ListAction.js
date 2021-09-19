a5365e1c6971d7d67ed0b4e422e3b996d4c63785
“Sanam” <pourmand2010@gmail.com>
2021-09-19 22:12:24 +0430

    committed by script

--- src/Components/List/ListAction.js
+++ src/Components/List/ListAction.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import HolismIcon from '../HolismIcon';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Holism from '../../Base/Holism';
 
-const ListAction = ({ icon, title, act }) => {
-    return <Button
-        variant="outlined"
-        startIcon={HolismIcon({ icon })}
-        onClick={act}
-        className='ml-2'
-    >
-        {title}
-    </Button>
+const ListAction = ({ icon, title, click }) => {
+
+    const [progress, setProgress] = useState(false);
+
+    const reloadList = () => {
+        Holism.emit(Holism.reloadRequirement)
+    }
+
+    return <span className="listAction"> {
+        (progress == true)
+            ?
+            <CircularProgress />
+            :
+            <Button
+                variant="outlined"
+                startIcon={HolismIcon({ icon })}
+                onClick={() => click({ setProgress, reloadList })}
+                className='ml-2'
+            >
+                {title}
+            </Button>
+    }
+    </span>
 };
 
 export { ListAction }
