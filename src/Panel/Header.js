d2754d0b673ca3d7937b4cbc5c57fb910828c432
Saeed Nemati <sn.1361@gmail.com>
2021-09-30 20:20:00 +0330

    committed by script

--- src/Panel/Header.js
+++ src/Panel/Header.js
@@ -11,6 +11,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 // import HeaderActions from '../HeaderActions.js'
 import { FullScreen } from './HeaderActions/FullScreen';
 import { Maximize } from './HeaderActions/Maximize';
+import Collapse from '@material-ui/core/Collapse';
 
 const Header = ({ onMenuIconClicked }) => {
 
@@ -52,28 +53,33 @@ const Header = ({ onMenuIconClicked }) => {
     });
 
     return <>
-        <div id='header' className={"flex items-center p-10 justify-between " + (true ? "h-20" : "h-0") + (isShown ? " " : " hidden")} >
-            <div>
-                <div className='bg-white rounded-md p-1.5 px-2.5 text-gray-600 cursor-pointer' onClick={onMenuIconClicked}>
-                    <MenuIcon />
+        <Collapse in={isShown}>
+            <div id='header' className="flex items-center p-10 justify-between h-20" >
+                <div>
+                    <div className='bg-white rounded-md p-1.5 px-2.5 text-gray-600 cursor-pointer' onClick={onMenuIconClicked}>
+                        <MenuIcon />
+                    </div>
+                </div>
+                <div className='flex items-center justify-center'>
+                    {
+                        // items.map((item, index) => <div onClick={item.onClick || (() => { })} key={item.name} className={(index === 0 ? "" : "ml-6 ") + 'text-gray-600 cursor-pointer hover:text-blue-500'}>
+                        //     {item.icon}
+                        // </div>)
+                        // <HeaderActions />
+                        <>
+                            <Maximize />
+                            <FullScreen />
+                        </>
+                    }
                 </div>
             </div>
-            <div className='flex items-center justify-center'>
-                {
-                    // items.map((item, index) => <div onClick={item.onClick || (() => { })} key={item.name} className={(index === 0 ? "" : "ml-6 ") + 'text-gray-600 cursor-pointer hover:text-blue-500'}>
-                    //     {item.icon}
-                    // </div>)
-                    // <HeaderActions />
-                    <>
-                        <Maximize />
-                        <FullScreen />
-                    </>
-                }
+        </Collapse>
+        <Collapse in={!isShown}>
+            <div
+                className="m-auto absolute top-0 right-0 left-0 h-0 flex justify-center" onClick={() => Holism.emit(Holism.returnBackToNormalForm)}>
+                <ExpandMoreIcon style={{ fontSize: 40 }} className="cursor-pointer" />
             </div>
-        </div>
-        <div className={"m-auto absolute top-0 right-0 left-0 h-0 flex justify-center " + (isShown ? " hidden" : "")} onClick={() => Holism.emit(Holism.returnBackToNormalForm)}>
-            <ExpandMoreIcon style={{ fontSize: 40 }} className="cursor-pointer" />
-        </div>
+        </Collapse>
     </>
 };
 
