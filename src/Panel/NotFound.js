04d73cc76cc83463bc201b8aaf8ca8e71db27465
Saeed Nemati <sn.1361@gmail.com>
2021-10-25 15:28:21 +0330

    committed by script

--- src/Panel/NotFound.js
+++ src/Panel/NotFound.js
@@ -11,7 +11,7 @@ const NotFound = () => {
 
     return <div className="flex flex-col items-center justify-center">
         <div className="text-9xl text-red-400 font-bold">404</div>
-        <div className="uppercase mt-10 text-6xl font-bold tracking-widest text-gray-600">NOT FOUND</div>
+        <div className="uppercase mt-10 text-6xl font-bold tracking-widest text-gray-600 text-center">NOT FOUND</div>
         <div className="text-sm mt-10 text-gray-600 font-light text-center">The page you requested does not exist.<br />Please use the menu to navigate.<br />Or go to the home page.</div>
         <div className="mt-10">
             <Link
