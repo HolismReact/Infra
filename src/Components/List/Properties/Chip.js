86190f5e71037b0f955a4cccd9ed4a51827391df
Saeed Nemati <sn.1361@gmail.com>
2021-11-27 20:49:19 +0330

    committed by script

--- src/Components/List/Properties/Chip.js
+++ src/Components/List/Properties/Chip.js
@@ -1,5 +1,5 @@
 const Chip = ({ text, className }) => {
-    return <span className={"py-1 px-3 rounded-full " + className}>
+    return <span className={"py-1 px-3 rounded-full text-xs " + className}>
         {text}
     </span>
 }
