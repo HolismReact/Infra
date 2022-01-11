6c6897ebd6bae45f4a1793582c753834c15de1fd
Saeed Nemati <sn.1361@gmail.com>
2022-01-11 21:00:28 +0330

    committed by script

--- src/Components/Form/Fields/Rte.js
+++ src/Components/Form/Fields/Rte.js
@@ -1,5 +1,24 @@
+import React, { useState } from 'react'
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+
 const Rte = () => {
-    return <div>RTE</div>
+    const [editor] = useState(() => withReact(createEditor()))
+    const [value, setValue] = useState([
+        {
+            type: 'paragraph',
+            children: [{ text: 'A line of text in a paragraph.' }],
+          },
+    ])
+    return <div>
+        <Slate
+            editor={editor}
+            value={value}
+            onChange={newValue => setValue(newValue)}
+        >
+            <Editable />
+        </Slate>
+    </div>
 }
 
 export { Rte }
