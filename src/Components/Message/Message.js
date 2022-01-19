e74648652b1b824c8349860825eb2b393d269ee1
Saeed Nemati <sn.1361@gmail.com>
2022-01-19 07:08:15 +0330

    committed by script

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import Alert from '@mui/material/Alert'
 import AlertTitle from '@mui/material/AlertTitle'
 import Collapse from '@mui/material/Collapse'
+import Button from '@mui/material/Button'
 
 const Message = ({
     severity,
     title,
     text,
     close,
-    action
+    action,
+    className
 }) => {
 
     const [isOpen, setIsOpen] = useState(true)
 
     return <Collapse
         in={isOpen}
+        className={className}
     >
         <Alert
             severity={severity}
-            action={action}
+            action={<Button
+                className="text-slate-800"
+            >{action}</Button>}
             onClose={close ? () => setIsOpen(false) : null}
+            className=""
         >
             {title && <AlertTitle>{title}</AlertTitle>}
             {text}
