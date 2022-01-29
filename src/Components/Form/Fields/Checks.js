8affa69269dd35fb7b4059871e28d36dc1398ff8
Saeed Nemati <sn.1361@gmail.com>
2022-01-29 16:35:50 +0330

    committed by script

---
+++ src/Components/Form/Fields/Checks.js
@@ -0,0 +1,15 @@
+import { app, FormContext, fieldStyles } from '@Form';
+
+const Checks = ({
+    column,
+    items,
+    checkedItems,
+    show,
+    choose
+}) => {
+    return <div className={fieldStyles}>
+
+    </div>
+}
+
+export { Checks }
