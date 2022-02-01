cc641e92e703d8a0e4a6e3a67621b1380518c9dd
Saeed Nemati <sn.1361@gmail.com>
2022-02-01 15:09:53 +0330

    committed by script

--- src/Components/Form/Fields/Upload.js
+++ src/Components/Form/Fields/Upload.js
@@ -1,8 +1,28 @@
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { app, Field } from '@Form';
 import { fieldStyles } from './FieldStyle'
 
-const Upload = () => {
-    return <div className={fieldStyles}>
-        
+const Upload = ({
+
+}) => {
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+    return <div
+        className={fieldStyles + " bg-slate-100 flex justify-center items-center py-20 cursor-pointer group hover:bg-slate-200 border-dashed border-2 border-slate-400 hover:border-slate-600"}
+        {...getRootProps()}
+    >
+        <input {...getInputProps()} />
+        <p className="text-sm tracking-wide font-bold text-slate-600 group-hover:drop-shadow group-hover:drop-shadow">
+            {
+                isDragActive ?
+                    <span>Drop the files here ...</span> :
+                    <span>Drag &amp; drop some files here, or click to select files</span>
+            }
+        </p>
     </div>
 }
 
