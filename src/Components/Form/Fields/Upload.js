335fe1ee82c8210a3faee8d6b6865ef9d21b5d35
Saeed Nemati <sn.1361@gmail.com>
2022-02-01 17:22:43 +0330

    committed by script

--- src/Components/Form/Fields/Upload.js
+++ src/Components/Form/Fields/Upload.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react'
 import IconButton from '@mui/material/IconButton';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useDropzone } from 'react-dropzone'
+import Fade from '@mui/material/Fade';
 import { app, Field } from '@Form';
 import { fieldStyles } from './FieldStyle'
 
@@ -10,6 +11,8 @@ const Upload = ({
 }) => {
     const [files, setFiles] = useState([])
     const [previews, setPreviews] = useState([])
+    const [hasImages, setHasImages] = useState(false)
+
     const onDrop = useCallback(acceptedFiles => {
         setFiles(acceptedFiles)
     }, [])
@@ -27,52 +30,52 @@ const Upload = ({
     }, [files])
 
     useEffect(() => {
-        console.log(previews)
+        setHasImages(previews.length > 0)
     }, [previews])
 
-    const removeImage = (preview) => {
+    const removeImage = (e, preview) => {
         setFiles(files.filter(i => i.name != preview.name))
+        e.stopPropagation()
     }
 
     return <div
         className={fieldStyles + (previews.length === 0 ? " relative bg-slate-100 flex justify-center items-center py-20 cursor-pointer group hover:bg-slate-200 border-dashed border-2 border-slate-400 hover:border-slate-600 " : "")}
         {...getRootProps()}
     >
-        {
-            previews.length > 0
-                ?
-                <div className="relative flex items-center justify-around">
+        <Fade in={hasImages}>
+            <div className="relative flex items-center justify-around">
+                {
+                    previews.map(preview => <div className="relative">
+                        <img
+                            className="rounded-lg shadow-md shadow-black w-36 h-36 object-cover "
+                            src={preview.url}
+                        />
+                        <IconButton
+                            className="absolute -top-4 -right-4 "
+                            aria-label="delete"
+                            onClick={(e) => removeImage(e, preview)}
+                        >
+                            <CancelIcon />
+                        </IconButton>
+                    </div>)
+                }
+            </div>
+        </Fade>
+        <Fade in={!hasImages}>
+            <div>
+                {
+                    isDragActive && <div className="absolute inset-0 bg-green-500 animate-pulse"></div>
+                }
+                <input {...getInputProps()} />
+                <p className="relative text-sm tracking-wide font-bold text-slate-600 group-hover:drop-shadow group-hover:drop-shadow">
                     {
-                        previews.map(preview => <div className="relative">
-                            <img
-                                className="rounded-lg shadow-md shadow-black w-20 h-20 object-cover "
-                                src={preview.url}
-                            />
-                            <IconButton
-                                className="absolute -top-4 -right-4 "
-                                aria-label="delete"
-                                onClick={() => removeImage(preview)}
-                            >
-                                <CancelIcon />
-                            </IconButton>
-                        </div>)
+                        isDragActive ?
+                            <span>Drop the {multiple ? "files" : "file"} here ...</span> :
+                            <span>Drag &amp; drop {multiple ? "some files" : "a file"} here, or click to select {multiple ? "files" : "a file"}</span>
                     }
-                </div>
-                :
-                <div>
-                    {
-                        isDragActive && <div className="absolute inset-0 bg-green-500 animate-pulse"></div>
-                    }
-                    <input {...getInputProps()} />
-                    <p className="relative text-sm tracking-wide font-bold text-slate-600 group-hover:drop-shadow group-hover:drop-shadow">
-                        {
-                            isDragActive ?
-                                <span>Drop the {multiple ? "files" : "file"} here ...</span> :
-                                <span>Drag &amp; drop {multiple ? "some files" : "a file"} here, or click to select {multiple ? "files" : "a file"}</span>
-                        }
-                    </p>
-                </div>
-        }
+                </p>
+            </div>
+        </Fade>
     </div>
 }
 
