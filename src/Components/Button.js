4fe113f640b83f95a00c8ef65f6ff6bae1c4d1ea
Saeed Nemati <sn.1361@gmail.com>
2022-02-07 18:12:21 +0330

    committed by script

--- src/Components/Button.js
+++
@@ -1,5 +0,0 @@
-import Button from '@mui/material/Button';
-
-const Button = ({ props }) => {
-
-}
--- src/Components/List/Properties/Chip.js
+++ src/Components/List/Properties/Chip.js
@@ -1,4 +1,7 @@
-const Chip = ({ text, className }) => {
+const Chip = ({ 
+    text, 
+    className
+}) => {
     return <span className={"py-1 px-3 rounded-full text-xs " + className}>
         {text}
     </span>
