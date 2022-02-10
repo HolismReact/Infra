c220c73bd3a568a66c376162691048da8cbde11f
Saeed Nemati <sn.1361@gmail.com>
2022-02-10 13:06:56 +0330

    committed by script

--- src/Components/List/Properties/ImageGroup.js
+++ src/Components/List/Properties/ImageGroup.js
@@ -3,16 +3,16 @@ const ImageGroup = ({
 }) => {
     const firstFiveUrls = urls?.slice(0, 5)
     console.log(urls, firstFiveUrls)
-    return <div className="imageGroup">
+    return <div className="imageGroup flex items-center justify-center">
         {
-            firstFiveUrls.map(url => <span className="w-8 h-8 inline-block rounded-full -ml-3" key={url}>
+            firstFiveUrls.map(url => <span className="w-8 h-8 inline-block rounded-full -ml-3 bg-white" key={url}>
                 <img className="w-full h-full object-cover rounded-full border" src={url} />
             </span>)
         }
         {
             urls.length > 5 &&
-            <span className="w-8 h-8 inline-block rounded-full border">
-                +{urls.length - 5}
+            <span className="w-8 h-8 inline-block rounded-full border -ml-3 z-10 bg-white flex items-center justify-center font-medium text-slate-600 text-xs">
+                {`+${urls.length - 5}`}
             </span>
         }
     </div>
