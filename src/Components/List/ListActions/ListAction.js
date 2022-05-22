3962fa43929339ec7fb0e35b99d25ecda8f29df4
Saeed Nemati <sn.1361@gmail.com>
2022-05-22 14:03:45 +0430

    committed by script

--- src/Components/List/ListActions/ListAction.js
+++ src/Components/List/ListActions/ListAction.js
@@ -1,11 +1,12 @@
 import React, { useState, useContext } from 'react';
-import { ListContext } from '../List';
-import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import { ListContext } from '../List';
 import app from '../../../Base/App';
 import HolismIcon from '../../HolismIcon';
 
-const ListAction = ({ icon, title, click, minCardinality }) => {
+const ListAction = ({ icon, text, title, click, minCardinality }) => {
 
     const [progress, setProgress] = useState(false);
     const { selectedItems } = useContext(ListContext);
@@ -14,31 +15,41 @@ const ListAction = ({ icon, title, click, minCardinality }) => {
         app.emit(app.reloadRequested)
     }
 
-    return <span className="listAction">
-        <Button
-            variant="outlined"
-            disabled={progress || (minCardinality && minCardinality > selectedItems.length)}
-            startIcon={
-                progress
-                    ?
-                    <CircularProgress
-                        size={20}
-                    />
-                    :
-                    <HolismIcon icon={icon} />
-                // <CircularProgress
-                //     variant="determinate"
-                //     value={100}
-                //     size={20}
-                // />
+    const button = <Button
+        variant="outlined"
+        disabled={progress || (minCardinality && minCardinality > selectedItems.length)}
+        startIcon={
+            progress
+                ?
+                <CircularProgress
+                    size={20}
+                />
+                :
+                <HolismIcon icon={icon} />
+            // <CircularProgress
+            //     variant="determinate"
+            //     value={100}
+            //     size={20}
+            // />
 
-            }
-            /* HolismIcon creates 321 error in production build */
-            onClick={() => click({ setProgress, reloadList })}
-            className='mr-2 mt-2 lg:mt-0'
-        >
-            {app.t(title)}
-        </Button>
+        }
+        /* HolismIcon creates 321 error in production build */
+        onClick={() => click({ setProgress, reloadList })}
+        className='mr-2 mt-2 lg:mt-0'
+    >
+        {app.t(text || title)}
+    </Button>
+
+    return <span className="listAction">
+        {
+            title
+                ?
+                <Tooltip title={app.t(title || "")}>
+                    {button}
+                </Tooltip>
+                :
+                button
+        }
     </span>
 };
 
