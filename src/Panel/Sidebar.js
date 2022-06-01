f9b382436eaf950a8321d7d9c37d6722723fb8d5
Saeed Nemati <sn.1361@gmail.com>
2022-06-01 16:15:22 +0430

    committed by script

--- src/Panel/Sidebar.js
+++ src/Panel/Sidebar.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import Collapse from '@mui/material/Collapse';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import useLocalStorageState from '../Base/UseLocalStorageState'
 import Branding from './Branding.js';
 import User from './User.js';
 import Menu from './Menu.js';
 import HolismIcon from '../Components/HolismIcon.js';
 
 export default function Sidebar({ onClick }) {
-    const [open, setOpen] = useState()
+    const [open, setOpen] = useLocalStorageState(true, 'brandingAndMenuIsShown')
 
     return (
         <div className="h-full pt-4 dark:bg-slate-900">
