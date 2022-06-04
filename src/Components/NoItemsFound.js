184897106517ddcbe90a071f4caaab92df4c8f29
Saeed Nemati <sn.1361@gmail.com>
2022-06-04 10:41:47 +0430

    committed by script

--- src/Components/NoItemsFound.js
+++ src/Components/NoItemsFound.js
@@ -1,6 +1,11 @@
 import { app } from '@Panel'
 
-const NoItemsFound = () => {
+const NoItemsFound = ({
+    icon,
+    title,
+    description,
+    ctaText
+}) => {
     return <div className={'py-10 text-2xl font-bold text-gray-600'}>{app.t("No items found")}</div>
 }
 
